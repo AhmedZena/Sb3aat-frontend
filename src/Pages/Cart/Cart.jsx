89726e7569801e9682cf4bf34659e067ae15a702
Caroline <carolinekaram2020@gmail.com>
2024-03-29 19:47:30 +0200

    Changes Cart and Footer

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -5,11 +5,10 @@ import { useSelector } from "react-redux";
 
 export default function Cart() {
   const [orders, setOrders] = useState([]);
+  const [initialTotalPrices, setInitialTotalPrices] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const { user } = useSelector((state) => state.user);
-
-  console.log({ user });
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -21,7 +20,6 @@ export default function Cart() {
           return;
         }
 
-        // Send token to server for verification
         const response = await axios.get(
           `${process.env.baseUrl}/orders/client/${user._id}`,
           {
@@ -31,82 +29,121 @@ export default function Cart() {
           }
         );
 
-        // Set orders data if request is successful
         if (response.data) {
           setOrders(response.data);
+          setInitialTotalPrices(response.data.map((order) => order.totalPrice));
           console.log(response.data);
         }
       } catch (error) {
         console.error("Error fetching orders:", error);
         setError("Error fetching orders");
       } finally {
-        setIsLoading(false); // Update loading state
+        setIsLoading(false);
       }
     };
 
-    // Call fetchOrders function
     fetchOrders();
   }, []);
 
-  // Render loading state
+  const handleNumOrderedChange = (index, value) => {
+    const updatedOrders = [...orders];
+    updatedOrders[index].numsOrdered = parseInt(value);
+    setOrders(updatedOrders);
+  };
+
+  const handleDeleteOrder = (index) => {
+    const updatedOrders = [...orders];
+    updatedOrders.splice(index, 1);
+    setOrders(updatedOrders);
+  };
+
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div className="text-center">Loading...</div>;
   }
 
-  // Render error message if there's an error
   if (error) {
-    return <div>Error: {error}</div>;
+    return <div className="text-center">Error: {error}</div>;
   }
 
-  // Render orders data
   return (
-    <div className="container">
-      <div className="cartHeading bg-[#f5f5f5] flex flex-col p-10 pl-1 pt-1 m-60 mb-20 justify-between items-center">
-        {/* Render your orders data */}
-        <table className="w-full m-auto table-auto">
+    <div className="container mx-auto px-4 mt-12 mb-4">
+      <div className="bg-gray-100 py-8 px-6 rounded-md shadow-lg">
+        <table className="w-full">
           <thead>
-            <tr className="h-40 text-center font-bold">
-              <th>Services</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>TotalPrice</th>
+            <tr className="border-b border-gray-300">
+              <th className="py-2">Services</th>
+              <th className="py-2">Price</th>
+              <th className="py-2">Quantity</th>
+              <th className="py-2">Total Price</th>
+              <th className="py-2">Actions</th>
             </tr>
           </thead>
           <tbody>
             {orders.map((order, index) => (
-              <tr key={index} className="h-40">
-                {order.service ? (
-                  <td>
-                    {order.service.title}
-                    <img className=" h-32" src={order.service.serviceImgSrc} />
-                  </td>
-                ) : order.course ? (
-                  <td>
-                    {order.course.Title}
-                    <img className=" h-32" src={order.course.CourseImg} />
-                  </td>
-                ) : null}
-                <td>{order.totalPrice}</td>
-                {order.service ? (
-                  <td>
-                    {order.numsOrdered}
-                    <input type="number" />
-                  </td>
-                ) : order.course ? (
-                  <td>
-                   {order.numsOrdered}
-                  </td>
-                ) : null}
-                <td>{order.isPaid}</td>
+              <tr key={index} className="border-b border-gray-300">
+                <td className="py-4">
+                  {order.service ? (
+                    <>
+                      <span className="font-semibold">
+                        {order.service.title}
+                      </span>
+                      <img
+                        className="w-32 h-20 mt-2 ml-2"
+                        src={order.service.serviceImgSrc}
+                        alt="Service"
+                      />
+                    </>
+                  ) : order.course ? (
+                    <>
+                      <span className="font-semibold">
+                        {order.course.Title}
+                      </span>
+                      <img
+                        className="w-32 h-20 mt-2 ml-2"
+                        src={order.course.CourseImg}
+                        alt="Course"
+                      />
+                    </>
+                  ) : null}
+                </td>
+                <td className="py-4 font-semibold">{initialTotalPrices[index]}$</td>
+                <td className="py-4">
+                  <select
+                    value={order.numsOrdered}
+                    onChange={(e) =>
+                      handleNumOrderedChange(index, e.target.value)
+                    }
+                    className="px-2 py-1 border border-gray-300 rounded-md bg-gray-50 focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                  >
+                    {[1, 2, 3, 4, 5].map((value) => (
+                      <option key={value} value={value}>
+                        {value}
+                      </option>
+                    ))}
+                  </select>
+                </td>
+                <td className="py-4 font-semibold">
+                  {order.totalPrice
+                    ? order.totalPrice * order.numsOrdered
+                    : "0"}
+                  $
+                </td>
+                <td className="py-4">
+                  <button
+                    onClick={() => handleDeleteOrder(index)}
+                    className="text-red-500 font-bold hover:text-red-700"
+                  >
+                    Delete
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
-
-        <div className="flex justify-center">
+        <div className="flex justify-center mt-6">
           <Link
             to="/pay"
-            className="bg-green-500 text-white px-4 py-2 rounded-md"
+            className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 transition duration-300"
           >
             Pay
           </Link>
